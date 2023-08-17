8e4c267abb5d16ba15116cdaffc69f0db0a8d716
mohamadhammoud <mohamad.hammoudk@gmail.com>
2023-08-18 00:20:14 +0300

    💥 added swap functionality

--- contracts/UniswapV2Pair.sol
+++ contracts/UniswapV2Pair.sol
@@ -3,17 +3,30 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./libraries/Math.sol";
+import "./libraries/UQ112x112.sol";
 
 contract UniswapV2Pair is ERC20, Math {
+    using UQ112x112 for uint224;
+
     uint256 constant MINIMUM_LIQUIDITY = 1000;
 
     address public token0;
     address public token1;
 
-    uint256 private reserve0;
-    uint256 private reserve1;
+    uint112 private reserve0;
+    uint112 private reserve1;
+    uint32 private blockTimestampLast;
+
+    uint256 public price0CumulativeLast;
+    uint256 public price1CumulativeLast;
 
     error InsufficientLiquidityMinted();
+    error InsufficientLiquidityBurned();
+    error InsufficientOutputAmount();
+
+    error InsufficientLiquidity();
+    error TransferFailed();
+    error InvalidK();
 
     constructor(
         address token0_,
@@ -47,18 +60,16 @@ contract UniswapV2Pair is ERC20, Math {
 
         _mint(msg.sender, liquidity);
 
-        _update(balance0, balance1);
-    }
-
-    function _update(uint256 balance0, uint256 balance1) private {
-        reserve0 = uint112(balance0);
-        reserve1 = uint112(balance1);
+        (uint112 reserve0_, uint112 reserve1_, ) = getReserves();
+        _update(balance0, balance1, reserve0_, reserve1_);
     }
 
     function burn() public {
         uint256 balance0 = IERC20(token0).balanceOf(address(this));
         uint256 balance1 = IERC20(token1).balanceOf(address(this));
-        uint256 liquidity = balanceOf[msg.sender];
+        uint256 liquidity = balanceOf(msg.sender);
+
+        uint256 totalSupply = totalSupply();
 
         uint256 amount0 = (liquidity * balance0) / totalSupply;
         uint256 amount1 = (liquidity * balance1) / totalSupply;
@@ -73,6 +84,64 @@ contract UniswapV2Pair is ERC20, Math {
         balance0 = IERC20(token0).balanceOf(address(this));
         balance1 = IERC20(token1).balanceOf(address(this));
 
-        _update(balance0, balance1);
+        (uint112 reserve0_, uint112 reserve1_, ) = getReserves();
+        _update(balance0, balance1, reserve0_, reserve1_);
+    }
+
+    function swap(uint256 amount0Out, uint256 amount1Out, address to) public {
+        if (amount0Out == 0 && amount1Out == 0)
+            revert InsufficientOutputAmount();
+
+        (uint112 reserve0_, uint112 reserve1_, ) = getReserves();
+
+        if (amount0Out > reserve0_ || amount1Out > reserve1_)
+            revert InsufficientLiquidity();
+
+        uint256 balance0 = IERC20(token0).balanceOf(address(this)) - amount0Out;
+        uint256 balance1 = IERC20(token1).balanceOf(address(this)) - amount1Out;
+
+        if (balance0 * balance1 < uint256(reserve0_) * uint256(reserve1_))
+            revert InvalidK();
+
+        _update(balance0, balance1, reserve0_, reserve1_);
+
+        if (amount0Out > 0) _safeTransfer(token0, to, amount0Out);
+        if (amount1Out > 0) _safeTransfer(token1, to, amount1Out);
+    }
+
+    function getReserves() public view returns (uint112, uint112, uint32) {
+        return (reserve0, reserve1, blockTimestampLast);
+    }
+
+    function _update(
+        uint256 balance0,
+        uint256 balance1,
+        uint112 reserve0_,
+        uint112 reserve1_
+    ) private {
+        unchecked {
+            uint32 timeElapsed = uint32(block.timestamp) - blockTimestampLast;
+
+            if (timeElapsed > 0 && reserve0_ > 0 && reserve1_ > 0) {
+                price0CumulativeLast +=
+                    uint256(UQ112x112.encode(reserve1_).uqdiv(reserve0_)) *
+                    timeElapsed;
+                price1CumulativeLast +=
+                    uint256(UQ112x112.encode(reserve0_).uqdiv(reserve1_)) *
+                    timeElapsed;
+            }
+        }
+
+        reserve0 = uint112(balance0);
+        reserve1 = uint112(balance1);
+        blockTimestampLast = uint32(block.timestamp);
+    }
+
+    function _safeTransfer(address token, address to, uint256 value) private {
+        (bool success, bytes memory data) = token.call(
+            abi.encodeWithSignature("transfer(address,uint256)", to, value)
+        );
+        if (!success || (data.length != 0 && !abi.decode(data, (bool))))
+            revert TransferFailed();
     }
 }
